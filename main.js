6b554852bb006e1cc4d0555e98957c258bcf3c2d
AyaGaafar95 <ayagafar88@gmail.com>
2024-10-23 07:26:09 +0300

    delete All

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ function getTotal() {
 // TODO: create Product
 let allproductsArray = [];
 if(localStorage.product != null){
-allproductsArray = JSON.parse(localStorage.product);
-}
+allproductsArray = JSON.parse(localStorage.product);//here i add what in local storage to the array
+}// TODO: JSON.parse(localStorage.product) ... converts the JSON  into the allproductsArray
 
 
  submit.onclick = function(){
@@ -45,10 +45,11 @@ allproductsArray = JSON.parse(localStorage.product);
     }
     // save localstorage
 allproductsArray.push(product);
-localStorage.setItem('product', JSON.stringify(allproductsArray)) // TODO: converts the allproductsArray into a JSON string... Since localStorage can only store strings,
+localStorage.setItem('product', JSON.stringify(allproductsArray)) //here i add what in array to local storage 
+// TODO: JSON.stringify(allproductsArray)....converts the allproductsArray into a JSON string... Since localStorage can only store strings,
     console.log(allproductsArray);
     clearInputsData();
-
+    showDataOnTable();
  }
 
 // clear inputs 
@@ -76,22 +77,47 @@ function showDataOnTable(){
                     <td>${allproductsArray[i].adds}</td>
                     <td>${allproductsArray[i].discount}</td>
                     <td>${allproductsArray[i].total}</td>
-                    <td>${allproductsArray[i].count}</td>
                     <td>${allproductsArray[i].category}</td>
                     <td><button id="update">update</button></td>
-                    <td><button id="delete">delete</button></td>
+                    <td><button id="delete" onclick='deleteProduct(${i})'>delete</button></td>
                 </tr>`
-                console.log(table);
+               
                 
     }
 
     document.getElementById('tbody').innerHTML= table
+    let deleteAllDiv = document.getElementById('deleteAll');
+    if(allproductsArray.length > 0){
+        deleteAllDiv.innerHTML = `
+         <button onclick="deleteAll()">Delete All</button>
+        `
+    }else {
+        deleteAllDiv.innerHTML = ``;
+    }
+
 }
 
 showDataOnTable();
 
-// count 
 // delete 
+function deleteProduct(i){
+    allproductsArray.splice(i,1);
+    localStorage.product = JSON.stringify(allproductsArray);
+    showDataOnTable();
+}
+
+
+// delete All 
+function deleteAll(){
+localStorage.clear();  // Clear the data from localStorage
+//********************************* */
+ // Clear the in-memory array
+  allproductsArray.splice(0); // Option 1:      Using splice(0) to clear the array in-place
+ allproductsArray.length = 0;    // Option 3:   Set the length to 0 (another alternative)
+ allproductsArray = [];  // Option 2:        Reassign the array to a new empty array (you can use this instead)
+//********************************* */
+showDataOnTable();      // Update the UI to reflect the changes (clear the table) 
+}
 // update
 // search
 // clean data
